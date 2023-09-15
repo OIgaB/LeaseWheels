2684e46885d22ad0e8ac88a31d80f42855e5991e
OIgaB <57020231+OIgaB@users.noreply.github.com>
2023-09-15 23:37:51 +0000

    Deploying to gh-pages from @ OIgaB/LeaseWheels@1c1ee609eb4809a16b1f9015cedcfd635562d3ae 🚀

--- static/js/983.84023578.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunklease_wheels=self.webpackChunklease_wheels||[]).push([[983],{983:function(e,a,l){l.r(a);var s=l(87),n=l(184);a.default=function(){return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)("h3",{children:"Hi there! I am a HomePage!"}),(0,n.jsx)("p",{children:(0,n.jsx)(s.OL,{to:"/catalogue",children:"Link to CataloguePage (click me)"})})]})}}}]);
-//# sourceMappingURL=983.84023578.chunk.js.map
