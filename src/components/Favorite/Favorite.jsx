7020d3ed3b7275e441300a5b96f1012b7845e74b
OIgaB <linkbolya@gmail.com>
2023-09-18 16:10:30 +0300

    empty favorite page styling

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -1,4 +1,5 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+import { NavLink } from 'react-router-dom';
 import { selectFavoriteCars } from "../../redux/selectors";
 import scss from '../../styles/index.module.scss';
 import { Card } from '../Card';
@@ -8,13 +9,25 @@ const Favorite = () => {
 
     return (
         <div className={scss.dashbordContainer}>
-            <ul className={scss.dashbordList}>
-                {favoriteCars.length > 0 && (
-                    favoriteCars.map((car) => (
-                        <Card key={car.id} car={car} />                           
-                    ))
-                )}
-            </ul>
+            <div className={scss.dashbordBlank}></div>
+            {favoriteCars.length > 0 ? (
+                <ul className={scss.dashbordList}>
+                    {favoriteCars.map((car) => (
+                        <Card key={car.id} car={car} />
+                    ))}
+                </ul>
+            ) : (
+                <div className={scss.dashbordEmptyContainer}>
+                    <div className={scss.dashbordEmptyOverlay}>
+                    <div className={scss.dashbordEmptyNavigation}>
+                        <h1>
+                            <NavLink to="/catalogue">Cataloge</NavLink>
+                        </h1>                        
+                    </div>
+                        <h3 className={scss.dashbordNote}>Discover the handpicked gems from our catalogue – your next adventure awaits!</h3>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,14 +1,8 @@
-import { NavLink } from 'react-router-dom';
 import { Favorite } from 'components/Favorite';
 
 const FavoritesPage = () => {
     return (
-      <>
         <Favorite />
-        <p>
-          <NavLink to="/catalogue">CatalogePage (click me)</NavLink>
-        </p>
-      </>
     );
   };
   export default FavoritesPage;
